1f18e77cf1d1b7467e8bf732448daac1fd2aa9a3
aziza65 <158258861+aziza65@users.noreply.github.com>
2024-02-28 11:46:00 +0300

    Created test_book_manager.py to test book_manager.py

---
+++ test_book_manager.py
@@ -0,0 +1,28 @@
+import unittest
+from book_manager import Book, BookManager
+
+class TestBookManager(unittest.TestCase):
+    def setUp(self):
+        self.manager = BookManager()
+        self.book1 = {'isbn': 33456, 'title': 'Book 1', 'author': 'Author 1'}
+        self.book2 = {'isbn': 12345, 'title': 'Book 2', 'author': 'Author 2'}
+        # Add setup for test books here
+
+    # Implement test methods here
+    def test_add_book(self):
+        self.manager.add_book(self.book1)
+        self.manager.add_book(self.book2)
+        self.assert(self.manager.list_books(), [self.book1, self.book2])
+
+    def test_remove_book(self):
+        self.manager.add_book(self.book1)
+        self.manager.remove_book(33456)
+        self.assert(self.manager.list_books(), [])
+
+    def test_remove_nonexistent_book(self):
+        self.manager.add_book(self.book1)
+        self.manager.remove_book(0987)
+        self.assert(self.manager.list_books(), [self.book1])
+
+if __name__ == '__main__':
+    unittest.main()
